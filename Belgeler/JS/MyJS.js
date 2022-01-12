c7accd9947c39142279dcfee7b3d501d083a42d9
Harun Ethem ÇALIŞ <harunethm@gmail.com>
2022-01-12 11:26:22 +0300

    Revert "Update MyJS.js"
    This reverts commit 49f11058e949e17cd38bb37f009c9bed36b3d121.

--- Belgeler/JS/MyJS.js
+++ Belgeler/JS/MyJS.js
@@ -1,25 +1,6 @@
-﻿
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
 });
-
-function showNotification(from, align) {
-    var color = Math.floor((Math.random() * 4) + 1);
-
-    $.notify({
-        icon: "tim-icons icon-bell-55",
-        message: "Welcome to <b>Black Dashboard</b> - a beautiful freebie for every web developer."
-
-    }, {
-        type: type[color],
-        timer: 8000,
-        placement: {
-            from: from,
-            align: align
-        }
-    });
-}
 
 function generateListItemForMenu(orderid, name, amount, paidamount, comment, price) {
     var item = '<li class="list-group-item">' +
